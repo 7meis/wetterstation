8288c69030965e819c59bb2075b13a128abba593
Patrick Mischler <patrick_mischler@icloud.com>
2021-09-14 19:39:34 +0200

    Added rainmeter.py and modifications in wind_direction.py and wind_v3.py

--- experimental/wind_direction.py
+++ experimental/wind_direction.py
@@ -4,6 +4,21 @@ import busio
 import digitalio
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+from influxdb import InfluxDBClient, SeriesHelper
+
+# InfluxDB Client & Helper Class
+client = InfluxDBClient('localhost', 8086, '', '', 'weather')
+
+class WindDirectionHelper(SeriesHelper):
+    class Meta:
+        client = client
+        series_name = 'weather.stats.{location}'
+        fields = ['winddir']
+        tags = ['location']
+        bulk_size = 5
+        autocommit = True
+
+
 
 # initialize SPI BUS
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -50,24 +65,33 @@ values = []
 #            3.0: 270.0,
 #            2.7: 315.0}
 
-voltList = {2.4: 'N',
-            1.3: 'NE',
-            0.3: 'E',
-            0.4: 'SE',
-            0.8: 'S',
-            1.8: 'SW',
-            3.0: 'W',
-            2.7: 'NW'}
+voltList = {1.8: 'N',
+            2.0: 'NNE',
+            0.7: 'NE',
+            0.8: 'ENE',
+            0.1: 'E',
+            0.3: 'ESE',
+            0.2: 'SE',
+            0.6: 'SSE',
+            0.4: 'S',
+            1.4: 'SSW',
+            1.2: 'SW',
+            2.8: 'WSW',
+            2.7: 'W',
+            2.9: 'WNW',
+            2.2: 'NW',
+            2.5: 'NNW'}
 
 
 
 while True:
     time.sleep(1)
-    print("Value: ", chan0.value)
-    print("Voltage: ", round(chan0.voltage,2))
+    #print("Value: ", chan0.value)
+    #print("Voltage: ", round(chan0.voltage,1))
     wind = round(chan0.voltage, 1)
     if not wind in voltList:
         print('unknown value ' + str(wind))
     else:
+        WindDirectionHelper(location='jaberg', winddir=str(voltList[wind]))
         print('found ' + str(wind) + ' ' + str(voltList[wind]))
 
