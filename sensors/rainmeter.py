963c3cc68dbe2be6ae180415f429d528a0eb172a
Patrick Mischler <patrick.mischler@students.ffhs.ch>
2021-12-07 18:10:52 +0100

    line 11

--- sensors/rainmeter.py
+++ sensors/rainmeter.py
@@ -8,7 +8,7 @@ rainSensor = Button(6)
 dataFile = "data/rainmeterData.yml"
 if(exists(dataFile)):
     with open(dataFile, 'r') as f:
-        data = yaml.load(f, Loader=yaml.SafeLoader)
+        data = conf = yaml.load(f, Loader=yaml.SafeLoader)
     print(yaml.dump(data))
 else:
     data = {
