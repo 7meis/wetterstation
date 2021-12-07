64f7c31ff020899d7c60d721e83e7cc9fb6ab446
Patrick Mischler <patrick.mischler@students.ffhs.ch>
2021-12-07 18:08:37 +0100

    changed quotes in rainmeter for data

--- sensors/rainmeter.py
+++ sensors/rainmeter.py
@@ -12,23 +12,23 @@ if(exists(dataFile)):
     yaml.dump(data)
 else:
     data = {
-        "BUCKET_MEASURE": 0.2794,
-        "previousMonth": 0,
-        "previousDayOfMonth": 0,
-        "previousHour": 0,
-        "dayCount": 0,
-        "hourCount": 0,
-        "monthCount": 0,
-        "bucketTipTotalCount": 0,
-        "bucketTipPreviousCount": 0
+        'BUCKET_MEASURE': 0.2794,
+        'previousMonth': 0,
+        'previousDayOfMonth': 0,
+        'previousHour': 0,
+        'dayCount': 0,
+        'hourCount': 0,
+        'monthCount': 0,
+        'bucketTipTotalCount': 0,
+        'bucketTipPreviousCount': 0
     }
 
 def bucketTipped():
     global data
-    data["dayCount"] +=1
-    data["hourCount"] +=1
-    data["monthCount"] +=1
-    data["bucketTipTotalCount"] +=1
+    data['dayCount'] +=1
+    data['hourCount'] +=1
+    data['monthCount'] +=1
+    data['bucketTipTotalCount'] +=1
     with open(dataFile, 'w') as f:
       yaml.dump(data, f, sort_keys=False, default_flow_style=False)
 
@@ -36,33 +36,33 @@ def bucketTipped():
 def monthValue():
     global data
     currentMonth = datetime.datetime.today().month
-    if(currentMonth == data["previousMonth"]):
-        return round(data["monthCount"] * data["BUCKET_MEASURE"], 4)
+    if(currentMonth == data['previousMonth']):
+        return round(data['monthCount'] * data["BUCKET_MEASURE"], 4)
     else:
-        data["previousMonth"] = currentMonth
-        data["monthCount"] = 0
+        data['previousMonth'] = currentMonth
+        data['monthCount'] = 0
         return 0
 
 
 def dayValue():
     global data
     currentDayOdMonth = datetime.datetime.today().day
-    if(currentDayOdMonth == data["previousDayOfMonth"]):
-        return round(data["dayCount"] * data["BUCKET_MEASURE"], 4)
+    if(currentDayOdMonth == data['previousDayOfMonth']):
+        return round(data['dayCount'] * data['BUCKET_MEASURE'], 4)
     else:
-        data["previousDayOfMonth"] = currentDayOdMonth
-        data["dayCount"] = 0
+        data['previousDayOfMonth'] = currentDayOdMonth
+        data['dayCount'] = 0
         return 0
 
 
 def hourValue():
     global data
     currentHour = datetime.datetime.now().hour
-    if(currentHour == data["previousHour"]):
-        return round(data["hourCount"] * data["BUCKET_MEASURE"], 4)
+    if(currentHour == data['previousHour']):
+        return round(data['hourCount'] * data['BUCKET_MEASURE'], 4)
     else:
-        data["previousHour"] = currentHour
-        data["hourCount"] = 0
+        data['previousHour'] = currentHour
+        data['hourCount'] = 0
         return 0
 
 
