6b29a6e738bbbae0402c21e91b4ddf97c0317ae0
Patrick Mischler <patrick.mischler@students.ffhs.ch>
2021-12-21 14:42:08 +0100

    add channel scan

--- experimental/bme280_grove.py
+++ experimental/bme280_grove.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 import board
+import adafruit_tca9548a
 from adafruit_bme280 import basic as adafruit_bme280
 
 """initialize I2C and bme280 sensor"""
 i2c = board.I2C()
+
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+for channel in range(8):
+    if tca[channel].try_lock():
+        print("Channel {}:".format(channel), end="")
+        addresses = tca[channel].scan()
+        print([hex(address) for address in addresses if address != 0x70])
+        tca[channel].unlock()
+
+exit;
+
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 
